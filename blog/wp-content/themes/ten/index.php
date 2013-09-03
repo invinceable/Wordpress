bfdca91f274e751b4af3d64ce3bfc15f1222f9df
Vincent <vincent@vlau.org>
2013-09-03 12:08:45 -0700

    wp3

---
+++ blog/wp-content/themes/ten/index.php
@@ -0,0 +1,108 @@
+<?php get_header(); ?>
+<div id="background-pattern"></div>
+
+<div class="main">
+	<div class="row">
+	
+		<div class="small-24 medium-20 large-20 columns small-centered callout">
+		
+		<h1 class="light">Plan your next big event! </h1>
+		<h5 class="light" style="margin-top:-10px">Get inspired. Connect with top local venues and service providers you can trust.</h5>
+		
+			<form>
+			  <div class="row signUp">
+			    <div class="large-18 columns small-centered">
+			      <div class="row collapse">
+			        <div class="small-24 medium-16 large-16 columns">
+			          <input type="text" placeholder="Hex Value">
+			        </div>
+			        <div class="small-24 medium-7 large-7 columns">
+			          <a href="#" class="button prefix">Sign Up with Email</a>
+			        </div>
+			      </div>
+			    </div>
+			  </div>
+			</form>		
+		
+		</div>
+	
+	</div>
+
+</div>
+
+<div class="row infoGrid" style="max-width:100%">
+			
+			<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">
+				  		
+				  <li class="helpGrid">
+						  <div class="helpGridInner">
+							  <div class="row">
+								  <div class="small-22 medium-22 large-22 columns small-centered">
+									  <div class="small-4 medium-4 large-24 columns">
+										  <p class="entype">&#59297;</p>
+										  </div>
+									  
+									  <div class="small-20 medium-20 large-24 columns pad">
+										  <h4>Find the Right Match</h4>
+										  <a class="link">Learn More</a>
+										  </div>
+										  
+								  </div>	  
+							  </div>
+						  </div>
+				  </li>
+
+				  <li class="helpGrid">
+						  <div class="helpGridInner">
+							  <div class="row">
+								  <div class="small-22 medium-22 large-22 columns small-centered">
+									  <div class="small-4 medium-4 large-24 columns">
+										  	<p class="entype">&#128101;</p>
+										  </div>
+									  
+									  <div class="small-20 medium-20 large-24 columns pad">
+										  	<h4>Get Inspired</h4>
+										  <a class="link">Learn More</a>
+										  </div>
+										  
+								  </div>	  
+							  </div>
+						  </div>
+				  </li>
+				  
+				  <li class="helpGrid">
+						  <div class="helpGridInner">
+							  <div class="row">
+								  <div class="small-22 medium-22 large-22 columns small-centered">
+									  <div class="small-4 medium-4 large-24 columns">
+										  	<p class="entype">&#128161;</p>
+										  </div>
+									  
+									  <div class="small-20 medium-20 large-24 columns pad">
+										  	<h4>Dedicated Community</h4>
+										  <a class="link">Learn More</a>
+										  </div>
+										  
+								  </div>	  
+							  </div>
+						  </div>
+				  </li>
+				  				  
+				  				  
+				  <li class="helpGrid show-for-medium-up">
+				  <div class="row">
+				 
+					  <div class="small-18 columns small-centered specialGrid">
+				 
+					 <p><strong>San Francisco City Hall</strong></p>
+					 <p>Event Venue</p>
+				 
+					  </div>
+					  
+				  </div>
+				  </li>
+				  
+				</ul>
+			</div>
+
+<?php get_footer(); ?>
